bd0489cbfe04c0acd153fa93846a1f46b1de9a2d
alexandr <alex98saratovtsev@gmail.com>
2022-09-08 16:10:54 +0400

    Added HomeWork64, 66, 68

---
+++ Task66/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumNumbers(int m, int n)
+{          
+    if (m == n) return m;                          
+    else if (m < n) return n + SumNumbers(m, n - 1);         
+    else return n + SumNumbers(m, n + 1);               
+}
+
+Console.Write("M = ");
+int numberOne = int.Parse(Console.ReadLine());
+
+Console.Write("N = ");
+int numberTwo = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Result, S = {SumNumbers(numberOne, numberTwo)}");
---
+++ Task68/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 3, n = 2 -> A(m,n) = 29
+
+int FunctionAkkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (m > 0 && n == 0) return FunctionAkkerman(m - 1, 1);
+    else if (m > 0 && n > 0) return FunctionAkkerman(m - 1, FunctionAkkerman(m, n - 1));
+    return FunctionAkkerman(m, n);
+}
+
+Console.Write("Введите первое неотрицательное число: ");
+int numberOne = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе неотрицательное число: ");
+int numberTwo = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Result, S = {FunctionAkkerman(numberOne, numberTwo)}");
