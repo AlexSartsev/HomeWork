1f1dd5a60364bb3ddf3c1b0823074c9b74c34e28
alexandr <alex98saratovtsev@gmail.com>
2022-09-11 22:24:50 +0400

    Added HomeWork6

---
+++ Task41/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите количество чисел: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Число №{i + 1}: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(int[] array) 
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++) 
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write(array[i]);
+    }
+    Console.Write("]");
+}
+
+void QuantityArrayNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    Console.Write($" -> {count}");
+}
+
+int[] arr = CreateArray(size);
+PrintArray(arr);
+QuantityArrayNumbers(arr);
---
+++ Task43/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 43: Напишите программу, 
+// которая найдёт точку пересечения 
+// двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Write("Введите значение b1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение k1: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение b2: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение k2: ");
+int number4 = Convert.ToInt32(Console.ReadLine());
+
+void IntersectionsOfPoints(double b1, double k1, double b2, double k2)
+{
+    double x = default;
+    double y = default;
+
+    x = (b2 - b1) / (k1 - k2);
+    y = k1 * (b2 - b1) / (k1 - k2) + b1;
+    
+    Console.WriteLine($"{x}, {y}");
+}
+
+IntersectionsOfPoints(number1, number2, number3, number4);
